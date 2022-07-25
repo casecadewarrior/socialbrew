846a7fa0cac33ce73e38915c440ba0152ba91e22
Shaun Parker <mail@shaunparker.me>
2022-07-25 23:35:26 +0100

    Added header component inside the compoent folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import Header from './components/header';
+
 function App() {
   return (
-    <div className="font-poppins container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
-    </div>
+    <Header/>
+    
   );
 }
 
