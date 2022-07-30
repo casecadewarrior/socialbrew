dd328b629d099083327e2aa2e37cd77a4ec12d24
Shaun Parker <mail@shaunparker.me>
2022-07-30 17:08:31 +0100

    add date file and create profile header to show correct data

--- src/components/profileHeader/index.js
+++ src/components/profileHeader/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import data from "../../data/user-data.js";
+const profileUser = data[2]; // sets user for the profile, this means we can access data connected to the user
+const bannerImage = profileUser.profileBannerImageSrc;
+
+const bannerBackgroundImage = {
+  backgroundImage: 'url(' + bannerImage + ')',
+};
+
+
+
+
 
 export default function ProfileHeader() {
+    console.log('Profile URL: ' + bannerImage);
     return (
     <>
-      <div className="bg-red w-full h-20">
-        
+      <div className="font-poppins bg-red w-full h-40 md:h-50 lg:h-48 bg-cover bg-center" style={bannerBackgroundImage}></div>
+      <div className="w-full relative bg-red p-4">
+        <div className="lg:container lg:mx-auto w-full flex items-center">
+          <img className="w-20 h-20 absolute bottom-5 rounded-full border-white border-2" src={profileUser.profileImageSrc} alt={profileUser.profileImageAlt}></img>
+          <div className="flex items-center ml-auto">  
+            <button className="mx-4 text-xs uppercase text-white font-bold">Follow</button>
+            <button className="mx-4 text-xs uppercase text-white font-bold">Message</button>
+          </div>
+        </div>
+      </div>
+      <div className="font-poppins bg-slate-100 w-full">
+        <div className="lg:container lg:mx-auto w-full p-4">
+          <h1 className="text-red font-bold">{profileUser.name}</h1>
+          <p className="mb-4 text-xs">@{profileUser.displayName}</p>
+          <p className="text-xs">{profileUser.bio}</p>
+        </div>
       </div>
     </>
   );
---
+++ src/data/user-data.js
@@ -0,0 +1,53 @@
+export default
+[
+    {
+        id: 1,
+        name: 'Shaun Parker',
+        displayName: 'displayNameShaun',
+        profileImageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        profileImageAlt: "Shauns profile",
+        profileBannerImageSrc: 'https://images.unsplash.com/photo-1506765515384-028b60a970df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFubmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60', 
+        profileBannerImageAlt: "Shauns banner image alt",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus."
+    },
+    {
+        id: 2,
+        name: 'Johana Parker',
+        displayName: 'displayNameJohana',
+        profileImageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        profileImageAlt: "Johanas image alt",
+        profileBannerImageSrc: 'https://images.unsplash.com/photo-1506765515384-028b60a970df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFubmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60', 
+        profileBannerImageAlt: "Johanas banner image",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus."
+    },
+    {
+        id: 3,
+        name: 'Tim Parker',
+        displayName: 'displayNameTim',
+        profileImageSrc: 'https://www.fillmurray.com/200/200',
+        profileImageAlt: "Tims profile image",
+        profileBannerImageSrc: 'https://www.fillmurray.com/640/360', 
+        profileBannerImageAlt: "Tims profile banner image",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus."
+    },
+    {
+        id: 4,
+        name: 'Aaron Parker',
+        displayName: 'displayNameAaron',
+        profileImageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        profileImageAlt: "Aarons profile image",
+        profileBannerImageSrc: 'https://www.fillmurray.com/640/360', 
+        profileBannerImageAlt: "Aarons profile banner image alt",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus."
+    },
+    {
+        id: 5,
+        name: 'Carl Parker',
+        displayName: 'displayNameCarl',
+        profileImageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        profileImageAlt: "Carls profile image alt",
+        profileBannerImageSrc: 'https://www.fillmurray.com/640/360', 
+        profileBannerImageAlt: "Carls profile banner image alt",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus."
+    },
+  ]
