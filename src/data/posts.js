a51c52d33ef37650a00c13f5c79f178adfedd117
Shaun Parker <mail@shaunparker.me>
2022-07-31 11:42:26 +0100

    Added new post data file

---
+++ src/data/posts.js
@@ -0,0 +1,31 @@
+export default
+[
+    {
+        id: 1,
+        authorId: 2,
+        creationTimeStamp: '2022-08-14T04:00:00Z',
+        authordisplayName: 'displayNameJohana',
+        postBodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus.',
+        postImageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        postImageAlt: "Shauns profile",
+    },
+    {
+        id: 2,
+        authorId: 2,
+        creationTimeStamp: '2022-08-14T04:00:00Z',
+        authordisplayName: 'displayNameJohana',
+        postBodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus.',
+        postImageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        postImageAlt: "Shauns profile",
+    },
+    {
+        id: 3,
+        authorId: 2,
+        creationTimeStamp: '2022-08-14T04:00:00Z',
+        authordisplayName: 'displayNameJohana',
+        postBodyText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin efficitur, leo ut molestie aliquet, felis nulla volutpat libero, non gravida nunc mi at ante. Phasellus sed magna blandit, auctor risus ac, aliquet tellus.',
+        postImageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        postImageAlt: "Shauns profile",
+    },
+    
+  ]
