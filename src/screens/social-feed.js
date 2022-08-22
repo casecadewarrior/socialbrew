3a3644244c1655f22fe3e6c11ebf20f3841665aa
Shaun Parker <mail@shaunparker.me>
2022-08-22 12:15:33 +0100

    add toggle functionality to recipe filter

--- src/screens/social-feed.js
+++ src/screens/social-feed.js
@@ -3,6 +3,13 @@ import Header from './../components/header';
 import SocialPosts from '../components/socialPosts';
 import SharePostForm from "../components/sharePostForm";
 
+/* 
+
+The logic to set the user needs to be created so the user
+can be sent to the social post component
+
+*/
+
 export default function SocialFeed() {
     return (
         <div>
