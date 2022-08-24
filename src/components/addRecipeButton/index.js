eb09c9bad85d2642d7c89efba5eb0777042fd36a
Shaun Parker <mail@shaunparker.me>
2022-08-24 14:31:04 +0100

    add new item to dashaboard list

--- src/components/addRecipeButton/index.js
+++ src/components/addRecipeButton/index.js
@@ -1,16 +1,12 @@
-import React from "react";
-
-function handleSubmit(e) {
-    e.preventDefault();
-    // Enter logic to save post to DB via API once ready
-    alert('Submitted');
-}
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 export default function sharePostForm() {
     return (
     <>
         <div className='w-full px-4 mb-4 font-poppins'>
-            <button className='bg-red rounded text-white p-2 text-sm hover:bg-redDark'>Add New Recipe</button>
+            <Link to="/about">
+                <button className='bg-red rounded text-white p-2 text-sm hover:bg-redDark'>Add New Recipe</button>
+            </Link>
         </div>
     </>
   );
--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -8,26 +8,30 @@ const dashboardItems = [
         verb: 'View',
         href: '/public-recipes',
         imageSrc: 'https://images.unsplash.com/photo-1505075106905-fb052892c116?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-        imageAlt: "#",
-        comingSoon: true,
     },
     {
         id: 2,
+        name: 'My Recipes',
+        verb: 'Work Out',
+        href: '/my-recipes',
+        imageSrc: 'https://images.unsplash.com/photo-1615332579037-3c44b3660b53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+        imageAlt: "#", 
+    },
+    {
+        id: 3,
         name: 'Social',
         verb: 'Gather',
         href: '/social',
         imageSrc: 'https://images.unsplash.com/photo-1532634922-8fe0b757fb13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80',
-        imageAlt: "#",
-        comingSoon: true, 
+        imageAlt: "#", 
     },
     {
-        id: 3,
+        id: 4,
         name: 'Calculators',
         verb: 'Work Out',
         href: '/brewing-calculators',
         imageSrc: 'https://images.unsplash.com/photo-1615332579037-3c44b3660b53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
         imageAlt: "#",
-        comingSoon: true, 
     },
   ]
   
@@ -37,7 +41,7 @@ export default function Dashboard() {
         <Header/>
         <div className="bg-white">
             {dashboardItems.map((item) => (
-            <div key={item.id} className="relative shadow-lg md:mb-2 bg-cover bg-center h-64 w-full flex flex-col" style={{ background: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${item.imageSrc})`,backgroundSize: 'cover' }}>
+            <div key={item.id} className="relative shadow-lg md:mb-2 bg-cover bg-center h-64 w-full flex flex-col" style={{ background: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${item.imageSrc})`,backgroundSize: 'cover',backgroundPosition: 'centre centre' }}>
                 <a href={item.href} className="w-full flex items-center justify-center flex-col h-64">
                     <h3 className="text-xl text-red bg-white py-3 px-5">
                         {item.name}
