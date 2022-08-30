6df662986d6528a9806cc3817173872333bdf51d
Shaun Parker <mail@shaunparker.me>
2022-08-30 20:31:45 +0100

    remove unused import

--- src/screens/user-profile.js
+++ src/screens/user-profile.js
@@ -5,7 +5,6 @@ import postData from "./../data/posts.js";
 import Header from './../components/header';
 import ProfileHeader from './../components/profileHeader';
 import SocialPosts from '../components/socialPosts';
-import SharePostForm from "../components/sharePostForm";
 
 //const profileUser = data[2]; // sets user for the profile, this means we can access data connected to the user
 const posts = postData;
